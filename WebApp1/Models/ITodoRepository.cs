6b78c7db13ab3b66c5cdef85a875b2c569751ed7
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-03 11:17:11 +0100

    Add a repository class

---
+++ WebApp1/Models/ITodoRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace WebApp1.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
