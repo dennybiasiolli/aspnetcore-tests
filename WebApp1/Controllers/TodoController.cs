3a1046c59cebfb2bb9ac827f509574b68dbc7b26
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-03 11:39:34 +0100

    Implement the other CRUD operations

--- WebApp1/Controllers/TodoController.cs
+++ WebApp1/Controllers/TodoController.cs
@@ -30,5 +30,46 @@ namespace WebApp1.Controllers
             return new ObjectResult(item);
         }
 
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
